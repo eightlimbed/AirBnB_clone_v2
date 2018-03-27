808840ae086f0f56c944c4d5909ef228aee042b9
Lee Gaines <eightlimbed@gmail.com>
2018-03-27 11:59:32 +0000

    adds script that installs nginx

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# This script sets up web servers for the deployment of web_static
+
+# Install nginx
+sudo apt-get update && apt-get install -y nginx
+
+# Create directories
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+
+# Create fake html file for testing if deployment worked
+sudo echo "success" > /data/web_static/releases/test/index.html
+
+# Create symlink called 'current' that links to the test folder
+# If it already exists, remove it
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# Give ownership of /data/ and all subdirectories to ubuntu
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update sites-enabled/default to to serve the contents of `current` from the
+# URL example.com/hbhb_static
+sudo sed -i \
+'42i location /hbnb_static/ {\n alias /data/web_static/current/;\n}' \
+/etc/nginx/sites-enabled/default
+
+sudo rm /etc/nginx/sites-available/default
+
+# Restart the server
+sudo service nginx restart
