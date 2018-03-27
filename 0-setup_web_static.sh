3bb53b3780883a1e669320d29c64685917320071
Lee Gaines <eightlimbed@gmail.com>
2018-03-27 17:43:40 +0000

    adds exec perms

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,10 +9,9 @@ sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test
 
 # Create fake html file for testing if deployment worked
-echo "success" | sudo tee /data/web_static/releases/test/index.html
+echo "Goal!" | sudo tee /data/web_static/releases/test/index.html
 
 # Create symlink called 'current' that links to the test folder
-# If it already exists, remove it
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Give ownership of /data/ and all subdirectories to ubuntu
@@ -21,10 +20,8 @@ sudo chown -R ubuntu:ubuntu /data/
 # Update sites-enabled/default to to serve the contents of `current` from the
 # URL example.com/hbhb_static
 sudo sed -i \
-'42i location /hbnb_static/ {\n alias /data/web_static/current/;\n}' \
+'42i location /hbnb_static {\n alias /data/web_static/current;\n}' \
 /etc/nginx/sites-enabled/default
-
-sudo rm /etc/nginx/sites-available/default
 
 # Restart the server
 sudo service nginx restart
