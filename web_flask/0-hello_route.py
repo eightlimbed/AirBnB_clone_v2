dc5cdac912ad5a84febd73bcccc84bcbeac8f061
Lee Gaines <eightlimbed@gmail.com>
2018-04-17 10:01:04 -0700

    style fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
